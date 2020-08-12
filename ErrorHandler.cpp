677a071ade739017ff7c730651b8b15d0beceb06
Kesha <affenmilchmann@gmail.com>
2020-08-12 04:30:53 +0300

    comments to ErrorHandler

--- ErrorHandler.cpp
+++ ErrorHandler.cpp
@@ -1,27 +1,33 @@
 #include "ErrorHandler.h"
 
+//Constructor
 ErrorHandler::ErrorHandler(sf::Font text_font)
 {
+	//takes outer font to init its fonts
 	this->text_font = text_font;
 	
+	//a bit weird but here error texts are written
 	messages = new std::string[10];
 	messages[0] = "while loading level.";
 	messages[1] = "while loading texture.";
 }
 
+//This func is called in main programm to make new error message (i think i must rename it lol)
 void ErrorHandler::getError(int code, int sec_time)
 {
 	//Making error message time stamp
 	timers.push_front(sec_time);
+	
 	//Making error message
 	errors.push_front(sf::Text());
-	
+	//Setting errors text settings
 	errors.front().setFont(text_font);
 	errors.front().setCharacterSize(char_size);
 	errors.front().setString("Error was orrured " + messages[code]);
 	errors.front().setPosition(sf::Vector2f(300, char_size * (errors.size() - 1)));
 }
 
+//This func is called in main draw loop (Session->drawFrame()) to draw error messanges
 void ErrorHandler::drawErrors(sf::RenderWindow* main_window)
 {
 	if (errors.empty()) return;
@@ -30,13 +36,16 @@ void ErrorHandler::drawErrors(sf::RenderWindow* main_window)
 		main_window->draw(t);
 }
 
+//This func is called in loop (Session->loop()) and it controls time messages are shown (and deletes is time had came to end)
 void ErrorHandler::errorHandlerUpdate(int sec_time)
 {
 	if (!timers.empty() && sec_time - timers.back() > life_time_seconds)
 	{
+		//deletting expiered one
 		timers.pop_back();
 		errors.pop_back();
 		
+		//moving others messages higher (does not work i dont know why)
 		int i = 0;
 		for (sf::Text t : errors)
 		{
--- ErrorHandler.h
+++ ErrorHandler.h
@@ -11,20 +11,32 @@
 class ErrorHandler 
 {
 public:
+	//Constructor
 	ErrorHandler(sf::Font text_font);
+	//Destructor
 	~ErrorHandler();
 
+	//Func to form new error message
 	void getError(int code, int sec_time);
+	//func to display errors
 	void drawErrors(sf::RenderWindow* main_window);
+	//func to delete expired ones
 	void errorHandlerUpdate(int sec_time);
+	
 private:
+	//string array for storing error texts
 	std::string* messages;
+	//errors are stored in sf::Text array
 	std::list<sf::Text> errors;
+	//array with timers of each error message
 	std::list<int> timers;
 	
+	//var to take Font from the main programm and set it sf::Texts
 	sf::Font text_font;
 	
+	//lifetime of each message in seconds
 	int life_time_seconds = 3;
 	
+	//size of messages texts
 	int char_size = 20;
 };
